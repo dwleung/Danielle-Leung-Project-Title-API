246d4180a2359ca44b8bd1ffd7f4c0af195f77f1
dani <118696363+dwleung@users.noreply.github.com>
2024-01-31 17:27:34 -0500

    Fix /openai/custom route to request custom idea

--- routes/openai_routes.js
+++ routes/openai_routes.js
@@ -4,8 +4,6 @@ import express from "express";
 const router = express.Router();
 dotenv.config();
 
-// const apiKey = process.env.OPENAI_API_KEY;
-
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
@@ -30,25 +28,25 @@ router.post("/", async (req, res) => {
 	res.status(200).send(response.choices[0].message);
 });
 
-//custom prompt
 router.post("/custom", async (req, res) => {
-	res.status.send("You've made it to openai/custom");
-	// const response = await openai.chat.completions.create({
-	// 	model: "gpt-3.5-turbo",
-	// 	messages: [
-	// 		{
-	// 			role: "system",
-	// 			content: "You will be provided with a set of interests and skills, and your task is to generate a software engineering project title, description, and 2-3 requirements using at least one interest and at least one skill.",
-	// 		},
-	// 		{
-	// 			role: "user",
-	// 			content: "",
-	// 		},
-	// 	],
-	// 	temperature: 0.8,
-	// 	max_tokens: 256,
-	// });
-	// res.status(200).console.log(response.choices[0].message);
+	console.log("You've made it to openai/custom");
+	console.log(req.body);
+	const response = await openai.chat.completions.create({
+		model: "gpt-3.5-turbo",
+		messages: [
+			{
+				role: "system",
+				content: "You will be provided with a set of interests and skills, and your task is to generate a software engineering project idea that includes at least one interest and at least one skill. Include maximum 3 requirements. Keep the response below 400 words. Format your response in a JSON object that includes the following keys: title, description, and requirements.",
+			},
+			{
+				role: "user",
+				content: `My interests are ${req.body.interests} and the skills I want to employ are ${req.body.skills}`,
+			},
+		],
+		temperature: 0.8,
+		max_tokens: 256,
+	});
+	res.status(200).send(response.choices[0].message);
 });
 
 export default router;
