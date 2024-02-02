44d31e970dcae540b5137e61b9e0465cef619f51
dani <118696363+dwleung@users.noreply.github.com>
2024-02-02 11:16:44 -0500

    Add chat history to reduce repeat responses

--- routes/openai_routes.js
+++ routes/openai_routes.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
 	const messagesArray = req.body;
 	messagesArray.unshift({
 		role: "system",
-		content: "Your task is to generate a software engineering project brief for a developer of a beginner skill level. The project should have 3 requirements that helps the user understand what to integrate in the project. Your response should be structured as a JSON object with keys: title, description, and requirements.",
+		content: "Your task is to generate a software engineering project brief. The project should have 3 requirements that helps the user understand what to integrate in the project. Your response should be structured as a JSON object with keys: title, description, and requirements.",
 	});
 
 	const response = await openai.chat.completions.create({
@@ -29,19 +29,17 @@ router.post("/", async (req, res) => {
 
 router.post("/custom", async (req, res) => {
 	console.log("You've made it to openai/custom");
-	console.log(req.body);
+	const messagesArray = req.body;
+	messagesArray.unshift({
+		role: "system",
+		content: "Your task is to generate a software engineering project brief for a developer of a beginner skill level. The project should have 3 requirements that helps the user understand what to integrate in the project. Your response should be structured as a JSON object with keys: title, description, and requirements.",
+	});
+
+	console.log("This is after unshift:", messagesArray);
+
 	const response = await openai.chat.completions.create({
 		model: "gpt-3.5-turbo",
-		messages: [
-			{
-				role: "system",
-				content: "You will be provided with a set of interests and skills, and your task is to generate a random software engineering project brief that uses the interests as seed words and employs the skills described. Include maximum 3 requirements that helps the user understand what to integrate in the project. Keep the response below 400 words. Format your response in a JSON object that includes the following keys: title, description, and features.",
-			},
-			{
-				role: "user",
-				content: `My interests are ${req.body.interests} and the skills I want to employ are ${req.body.skills}`,
-			},
-		],
+		messages: messagesArray,
 		temperature: 0.8,
 		max_tokens: 256,
 	});
