67105b71207d9eee1584a0a0b5cb2cd78e2c05bb
dani <118696363+dwleung@users.noreply.github.com>
2024-02-05 02:28:37 -0500

    Add function to save and fetch ideas

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,8 +4,45 @@ import knexfile from "../knexfile.js";
 import jwt from "jsonwebtoken";
 dotenv.config();
 const knex = knexLibrary(knexfile);
+const SECRET_KEY = process.env.SECRET_KEY;
 
-console.log("key", process.env.SECRET_KEY);
+function authorize(req, res, next) {
+	if (!req.headers.authorization) {
+		return res
+			.status(201)
+			.send("Authorization token not included. Please log in.");
+	}
+	const token = req.headers.authorization.split(" ")[1];
+	try {
+		const decoded = jwt.verify(token, SECRET_KEY);
+		req.user = decoded;
+		next();
+	} catch (error) {
+		res.status(400).json({
+			message: `Invalid authorization token: ${error}`,
+		});
+	}
+}
+
+// A Profile end-point that will return user information,
+// in this example, the user's name that they provided
+// when they signed up.
+// The authorize middleware function must check for
+// a token, verify that the token is valid, decode
+// the token and put the decoded data onto req.decoded
+const getProfile = async (req, res) => {
+	console.log("req.user", req.user);
+	const authUser = req.user;
+	try {
+		const user = await knex("users").where({ id: authUser.id }).first();
+		console.log("User found in database:", user);
+		const { id, name, username } = user;
+		res.status(200).json({ id, name, username });
+	} catch (error) {
+		return res.status(400).json({ message: `User not found: ${error}` });
+	}
+};
+
 const signup = async (req, res) => {
 	const { username, name, password } = req.body;
 
@@ -34,18 +71,19 @@ const login = async (req, res) => {
 		const foundUser = await knex("users").where({ username: username });
 		if (foundUser.length === 0) {
 			return res.status(404).json({
-				message: `User with username ${username} not found`,
+				message: `User "${username}" not found. Please sign up!`,
 			});
 		}
 		const user = foundUser[0];
+		console.log("This is my foundUser", user);
 
 		if (user && user.password === password) {
 			const token = jwt.sign(
-				{ username: foundUser.username },
-				process.env.SECRET_KEY,
+				{ id: user.id, name: user.name },
+				SECRET_KEY,
 				{ expiresIn: "24h" }
 			);
-			res.status(200).json({ token: token });
+			res.status(200).json({ id: user.id, token: token });
 		} else if (user.password != password) {
 			console.log("the passwords do not match");
 			res.status(400).json({
@@ -63,10 +101,88 @@ const login = async (req, res) => {
 // 	.route("/:id/ideas")
 
 // 	.post(userController.saveIdea)
-const saveIdea = async (req, res) => {};
-// 	.post(userController.savePrompt)
-const savePrompt = async (req, res) => {};
-// 	.get(userController.getIdeas);
-const getIdeas = async (req, res) => {};
+const saveIdea = async (req, res) => {
+	const { user_id, title, description, requirements } = req.body;
 
-export default { signup, login, saveIdea, savePrompt, getIdeas };
+	try {
+		const newIdeaId = await knex("ideas").insert({
+			user_id,
+			title,
+			description,
+			requirements,
+		});
+
+		console.log(newIdeaId);
+		const newIdea = await knex("ideas").where({ id: newIdeaId[0] });
+
+		res.status(201).json(newIdea);
+	} catch (error) {
+		res.status(500).json({
+			message: `Unable to save idea: ${error}`,
+		});
+	}
+};
+
+// 	.get(userController.getIdeas);
+const getIdeas = async (req, res) => {
+	const userId = req.params.id;
+	console.log("This is the params userID: ", userId);
+	try {
+		const data = await knex("ideas")
+			.select("title", "description", "requirements")
+			.where("user_id", userId);
+		res.status(200).json(data[0]);
+	} catch (error) {
+		res.status(404).json({
+			message: `Error retrieving prompts: ${error}`,
+		});
+	}
+};
+// 	.post(userController.savePrompt)
+const savePrompt = async (req, res) => {
+	const { user_id, interests, skills, toggles } = req.body;
+
+	try {
+		const newPromptId = await knex("prompts").insert({
+			user_id,
+			interests,
+			skills,
+			toggles,
+		});
+
+		console.log(newPromptId);
+		const newPrompt = await knex("prompts").where({ id: newPromptId[0] });
+
+		res.status(201).json(newPrompt);
+	} catch (error) {
+		res.status(500).json({
+			message: `Unable to save prompts: ${error}`,
+		});
+	}
+};
+
+const getPrompts = async (req, res) => {
+	const userId = req.params.id;
+	console.log("This is the params userID: ", userId);
+	try {
+		const data = await knex("prompts")
+			.select("interests", "skills", "toggles")
+			.where("user_id", userId);
+		res.status(200).json(data[0]);
+	} catch (error) {
+		res.status(404).json({
+			message: `Error retrieving prompts: ${error}`,
+		});
+	}
+};
+
+export default {
+	signup,
+	login,
+	authorize,
+	getProfile,
+	saveIdea,
+	getIdeas,
+	savePrompt,
+	getPrompts,
+};
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,13 +3,19 @@ const router = express.Router();
 import userController from "../controllers/user-controller.js";
 
 router.route("/signup").post(userController.signup);
-
 router.route("/login").post(userController.login);
+router
+	.route("/profile")
+	.get(userController.authorize, userController.getProfile);
 
 router
 	.route("/:id/ideas")
 	.post(userController.saveIdea)
-	.post(userController.savePrompt)
 	.get(userController.getIdeas);
+
+router
+	.route("/:id/prompts")
+	.post(userController.savePrompt)
+	.get(userController.getPrompts);
 
 export default router;
