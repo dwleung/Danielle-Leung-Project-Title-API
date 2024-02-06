7164e9431c41ecbb5d97819b5bcc880829921c12
dani <118696363+dwleung@users.noreply.github.com>
2024-02-05 22:10:54 -0500

    Combine OpenAI POST request to one, redundant

--- routes/openai_routes.js
+++ routes/openai_routes.js
@@ -8,16 +8,19 @@ const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
 
+// POST request to OpenAI API for project idea
 router.post("/", async (req, res) => {
 	try {
 		const messagesArray = req.body;
+		// Add the system role to the chat history that is provided to OpenAI API
+		// Math.random() adds noise to the prompts resulting in better variety
 		messagesArray.unshift({
 			role: "system",
-			content: `Your task is to generate a software engineering project idea based on popular interests or leading industries. ${Math.random()}The project should have 3 requirements that helps the user understand what to integrate in the project. The project should have a paragraph description indicating the project's impact, intended audience, and any other features.  Your response should be structured as a object with the keys: title, description, and requirements.${Date()}`,
+			content: `Your task is to generate a software engineering project idea for a developer of beginner skill level. The project should have 3 requirements that helps the user understand what to integrate in the project. The project should have a paragraph description indicating the project's impact, intended audience, and any other features.  Your response should be structured as a JSON object with keys: title, description, and requirements(which is an array of 3 sentences).${Math.random()}`,
 		});
 
 		const response = await openai.chat.completions.create({
-			model: "gpt-4-turbo",
+			model: "gpt-3.5-turbo-0125",
 			messages: messagesArray,
 			temperature: 0.8,
 			presence_penalty: 1.4,
@@ -30,27 +33,6 @@ router.post("/", async (req, res) => {
 			message: `Unable to generate idea: ${error}`,
 		});
 	}
-});
-
-router.post("/custom", async (req, res) => {
-	console.log("You've made it to openai/custom");
-	const messagesArray = req.body;
-	messagesArray.unshift({
-		role: "system",
-		content: "Your task is to generate a software engineering project idea for a developer of beginner to medium skill. The project should have 3 requirements that helps the user understand what to integrate in the project. The project should have a paragraph description indicating the project's impact, intended audience, and any other features.  Your response should be structured as a JSON object with keys: title, description, and requirements.",
-	});
-
-	console.log("This is after unshift:", messagesArray);
-
-	const response = await openai.chat.completions.create({
-		model: "gpt-4-turbo",
-		messages: messagesArray,
-		temperature: 0.8,
-		presence_penalty: 1.4,
-		frequency_penalty: 1.8,
-		response_format: { type: "json_object" },
-	});
-	res.status(200).send(response.choices[0].message);
 });
 
 export default router;
